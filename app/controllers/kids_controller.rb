018d0ada0e46581d197e917717564e71ae2246e5
taylorvercillo <taylornvcasey@gmail.com>
2022-07-15 17:42:35 -0600

    kids_controller

---
+++ app/controllers/kids_controller.rb
@@ -0,0 +1,34 @@
+class KidsController < ApplicationController
+    
+    get "/kids" do
+        kids = Kid.all
+        kids.to_json(include: [:memories])
+      end
+    
+      get "/kids/:id" do 
+        kids = Kid.find_by_id(params[:id])
+        kids.to_json(include: [:memories])
+      end
+    
+      post "/kids" do 
+        kids = Kid.create(
+          name: params[:name], 
+          age: params[:age],
+          img_src: params[:img_src]
+          )
+        kids.to_json(include: [:memories])
+      end
+    
+      patch "/kids/:id" do 
+        kid = Kid.find_by_id(params[:id])
+        kid.update(params)
+        kid.to_json
+      end
+    
+      delete "/kids/:id" do 
+        kid = Kid.find(params[:id])
+        kid.destroy
+        kid.to_json
+      end
+    
+end
