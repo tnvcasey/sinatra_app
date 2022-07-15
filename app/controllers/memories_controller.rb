cf976a98eb8917428d4b30a1613b77eab7f15f9f
taylorvercillo <taylornvcasey@gmail.com>
2022-07-15 17:43:14 -0600

    memories_controller

---
+++ app/controllers/memories_controller.rb
@@ -0,0 +1,43 @@
+class MemoriesController < ApplicationController
+
+    get "/memories" do 
+    memories = Memory.all
+    memories.to_json(include: [:kid])
+  end
+
+  get "/memories/:id" do 
+    memories = Memory.find_by_id(params[:id])
+    memories.to_json(include: [:kid])
+  end
+
+  post "/memories" do 
+    kid = Kid.find_by(id: params[:kid_id])
+    memory = kid.memories.create(
+      body: params[:body], 
+      date: params[:date], 
+      #kid_id: params[:kid_id]
+    )
+    memory.to_json
+  end
+
+
+  patch "/memories/:id" do 
+    memory = Memory.find_by_id(params[:id])
+    memory.update(body: params[:body])
+    memory.to_json(include: [:kid])
+  end
+
+  delete "/memories/:id" do 
+    memory = memory.find_by_id(params[:id])
+    memory.destroy
+    memory.to_json(include: [:kid])
+  end
+
+  get "/kids/:kid_id/memories" do 
+    kid = Kid.find_by_id(params("kid_id"))
+    kid.memories.to_json(include: [:kid])
+  end
+
+  
+    
+end
